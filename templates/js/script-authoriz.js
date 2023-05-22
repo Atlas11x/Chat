d9400207c397ac2ee04bc6b2d3868c8c8ff61e90
Armybed2 <safgatash@mail.ru>
2023-05-22 10:49:47 +0500

    authoriz-form-neon-update-1.0

--- templates/js/script-authoriz.js
+++ templates/js/script-authoriz.js
@@ -1,10 +1,30 @@
-var settingsIcon = document.querySelector('.settings-icon');
-var dropdownMenu = document.querySelector('.dropdown-menu');
+var toggleIcon = document.querySelector('.toggle-icon');
+var colorWave = document.querySelector('.color-wave');
+var colorList = document.querySelector('.color-list');
+var submitButton = document.querySelector('input[type="submit"]');
 
-settingsIcon.addEventListener('click', function() {
-  dropdownMenu.classList.toggle('show');
+toggleIcon.addEventListener('click', function() {
+  colorList.style.display = colorList.style.display === 'none' ? 'block' : 'none';
+});
+
+colorList.addEventListener('click', function(event) {
+  var color = event.target.dataset.color;
+  if (color) {
+    changeColor(color);
+    colorList.style.display = 'none';
+  }
 });
 
 function changeColor(color) {
-  document.body.style.backgroundColor = color;
+  submitButton.style.backgroundColor = color;
+  colorWave.style.opacity = 1;
+  colorWave.style.backgroundImage = `linear-gradient(to right, #03e9f4, ${color}, #ffeb3b)`;
+
+  setTimeout(function() {
+    colorWave.style.opacity = 0;
+    submitButton.classList.add('neon');
+    setTimeout(function() {
+      submitButton.classList.remove('neon');
+    }, 1000);
+  }, 2000);
 }
